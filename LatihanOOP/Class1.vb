a2b287bdee03ab689af33206cbcfe59577e2b1bf
Thawinatt A L Asian <18DDT19F1019@student.ptss.edu.my>
2021-05-04 17:10:53 +0800

    Add project files.

---
+++ LatihanOOP/Class1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Pelajar
+
+    Private strNoMatrik As String
+    Private strKodPoli As String
+    Sub New(ByVal strNoMatrik As String)
+        Me.strNoMatrik = strNoMatrik
+
+    End Sub
+
+
+    Public Function Politeknik() As String
+        Dim strKodPoli As String =
+        strNoMatrik.Substring(0, 2)
+
+        If strKodPoli = "18" Then
+            Return "PTSS"
+        Else
+
+            Return "Bukan PTSS"
+        End If
+
+    End Function
+
+    Public Function Program() As String
+        Dim strKodPoli As String =
+       strNoMatrik.Substring(0, 2)
+
+        If strKodPoli = "DDT" Then
+            Return "Diploma Teknologi Maklumat ( Teknologi Digital )"
+        Else
+
+            Return "Bukan DDT"
+        End If
+    End Function
+    Public Function Ambilan() As String
+        Dim strKodAmbilan As String = strNoMatrik.Substring(5, 3)
+        Dim strKodTahun As String = strNoMatrik.Substring(8, 1)
+        If strKodAmbilan = "19F" Then
+            If strKodTahun = "1" Then
+                Return "Jun 2019"
+            ElseIf strKodTahun = "2" Then
+                Return "Dec 2019"
+            End If
+        Else
+            Return "Bukan Sesi 2019"
+        End If
+
+    End Function
+
+End Class
